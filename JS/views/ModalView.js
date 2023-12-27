540681ffa65dbcfacb2f26a0956e5fc993d35d17
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-12-27 18:30:51 +0900

    fix: fix form name

--- JS/views/ModalView.js
+++ JS/views/ModalView.js
@@ -1,7 +1,7 @@
 export const ModalView = ({ title, placeholder, cancel, submit }) => {
     return `<h3 class="modal__title">${title}</h3>
     <form method="dialog" class="modal__form">
-        <textarea id="sentence" name="sentence" type="text" placeholder="${placeholder}" required></textarea>
+        <textarea id="sentence" name="text" type="text" placeholder="${placeholder}" required></textarea>
         <menu class="modal__form__btn-wrapper">
             <button type="button" class="modal__form--close">${cancel}</button>
             <button type="submit" class="modal__form--confirm">${submit}</button>
