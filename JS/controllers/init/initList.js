08af99e9ea768204cc0d06a07887b8085ae85476
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-12-27 17:57:56 +0900

    feat: add list event listener

--- JS/controllers/init/initList.js
+++ JS/controllers/init/initList.js
@@ -1,5 +1,13 @@
 import { renderList } from "../../views/render.js";
+import { onListClick } from "../event/onClick.js";
+
+const itemList = document.querySelector(".read-list");
+
+const initEventListeners = () => {
+    itemList.addEventListener("click", onListClick);
+};
 
 export const initList = () => {
     renderList("todo");
+    initEventListeners();
 };
