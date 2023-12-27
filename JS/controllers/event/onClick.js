1825e6c08b4e98911869dea3201b64180f692b61
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-12-27 17:53:28 +0900

    feat: add click handlers

--- JS/controllers/event/onClick.js
+++ JS/controllers/event/onClick.js
@@ -1,4 +1,35 @@
-import { closeDialog } from "../handlers/modalhandler.js";
+import { addItemHandler } from "../handlers/addItemHandler.js";
+import { deleteItemHandler } from "../handlers/deleteItemHandler.js";
+import { closeDialog } from "../handlers/modalHandler.js";
+import { openItemHandler } from "../handlers/openItemHandler.js";
+import { readItemHandler } from "../handlers/readHandler.js";
+import { updateItemHandler } from "../handlers/updateItemHandler.js";
+
+const clickHandlerMap = {
+    item__text: openItemHandler,
+    "add-item": addItemHandler,
+    "delete-item": deleteItemHandler,
+    "update-item": updateItemHandler,
+    "read-item": readItemHandler,
+};
+
+export const onMenuClick = (event) => {
+    const { target } = event;
+    const executeHandler = clickHandlerMap[target.classList[0]];
+    if (executeHandler) {
+        executeHandler(target);
+    }
+};
+
+export const onListClick = (event) => {
+    const { target } = event;
+    console.log(target);
+    const executeHandler = clickHandlerMap[target.classList[0]];
+
+    if (executeHandler) {
+        executeHandler(target);
+    }
+};
 
 export const onModalClick = (event) => {
     const { target } = event;
