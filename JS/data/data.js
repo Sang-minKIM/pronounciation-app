776616313c895e03f483264f33d17701bf4593be
Sang-minKIM <kimpran@naver.com>
2023-12-28 22:45:17 +0900

    chore: remove annotation

--- JS/data/data.js
+++ JS/data/data.js
@@ -2,14 +2,18 @@ const storageListData = localStorage.getItem("listData");
 const storageItemData = localStorage.getItem("itemData");
 
 export const listData = storageListData
-    ? JSON.parse(storageListData)
-    : {
-          todo: ["item1"],
-          /*will update
+  ? JSON.parse(storageListData)
+  : {
+      todo: ["item1"],
+      /*will update
         done:[]
     */
-      };
-export const itemData = storageItemData ? JSON.parse(storageItemData) : { item1: { text: "간장공장 공장장은 강공장장이고, 된장공장 공장장은 공 공장장이다.", id: "item1" } };
-
-// if (storageListData) listData[todo] = JSON.parse(storageListData[todo]);
-// if (storageItemData) itemData = JSON.parse(storageItemData);
+    };
+export const itemData = storageItemData
+  ? JSON.parse(storageItemData)
+  : {
+      item1: {
+        text: "간장공장 공장장은 강공장장이고, 된장공장 공장장은 공 공장장이다.",
+        id: "item1",
+      },
+    };
