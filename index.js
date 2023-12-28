05442f71079a04b773bf4f7420936ae0734b7466
Sang-minKIM <kimpran@naver.com>
2023-12-28 22:51:41 +0900

    fix: make body height responsive

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ import { initList } from "./JS/controllers/init/initList.js";
 import { initModal } from "./JS/controllers/init/initModal.js";
 import { initMenu } from "./JS/menu.js";
 
+function setScreenSize() {
+  //먼저 뷰포트 높이를 얻고 1%를 곱하여 vh 단위 값을 얻습니다.
+  let vh = window.innerHeight * 0.01;
+  //그런 다음 --vh 사용자 정의 속성의 값을 문서의 루트로 설정합니다.
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+window.addEventListener("resize", setScreenSize);
+
 (function () {
-    initMenu();
-    initList();
-    initModal();
+  initMenu();
+  initList();
+  initModal();
 })();
